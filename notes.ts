42427fb08792c3d32c43e08bb165dc61b6297820
Alessandro Marchesin <alessandromarchesin@gmail.com>
2025-02-07 14:31:32 +0100

    chore: add zod dependency and enhance notes with generic type examples

--- notes.ts
+++ notes.ts
@@ -56,10 +56,10 @@ type Result2 = ReturnType<() => string>// return type does the same but for func
 
 // I can combine the two
 type ResultCombines = GetPromiseReturnType<
-    () => Promise<{
-      firstName: string
-      lastName: string
-    }>
+  () => Promise<{
+    firstName: string
+    lastName: string
+  }>
 >
 
 // I cannot pass anything to return type. if you check the definition type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any; there is a constraint. that is why we added the extension.
@@ -69,3 +69,107 @@ type Result3 = ReturnType<number>
 class Human {
   name: string = 'Alessandro'
 }
+
+// 6 using record to specify type extension
+const getKeyWithHighestValue = <TObj extends Record<string, number>>(
+  obj: TObj,
+): {
+  key: keyof TObj
+  value: number
+} => {
+  const keys = Object.keys(obj) as Array<keyof TObj>
+
+  let highestKey: keyof TObj = keys[0]
+  let highestValue = obj[highestKey]
+
+  for (const key of keys) {
+    if (obj[key] > highestValue) {
+      highestKey = key
+      highestValue = obj[key]
+    }
+  }
+
+  return {
+    key: highestKey,
+    value: highestValue,
+  }
+}
+
+const resultKey = getKeyWithHighestValue({ a: 1, b: 2, c: 3 })
+
+const key = resultKey.key
+const value = resultKey.value
+
+// 7 sometimes you'll need to override the types inside the generic function with an assertion.
+const typedObjectKeys = <TObj extends object>(
+  obj: TObj,
+): Array<keyof TObj> => {
+  // @ts-expect-error: I know that it is going to be an array, i convert it as below
+  return Object.keys(obj)
+}
+
+const correctTypedObjectKeys = <TObj extends object>(
+  obj: TObj,
+) => {
+  return Object.keys(obj) as Array<keyof TObj>
+}
+
+const typedResult1 = typedObjectKeys({ name: 'Alessandro', age: 14 })
+const typedResult2 = correctTypedObjectKeys({ name: 'Alessandro', age: 14 })
+
+// 8 it is actually returning the type of the value of the specified key. arument key is not treated as a type level but as "a" or "b". We need multiple generics.
+const getValue = <TObj>(obj: TObj, key: keyof TObj) => {
+  return obj[key]
+}
+
+const getValueCorrect = <TObj, TKey extends keyof TObj>(obj: TObj, key: TKey) => {
+  return obj[key]
+}
+
+const resultingValue = getValue(
+  {
+    a: 1,
+    b: 'some-string',
+  },
+  'a',
+)
+
+const resultingValueCorrect = getValueCorrect(
+  {
+    a: 1,
+    b: 'some-string',
+  },
+  'a',
+)
+
+// 9 default generics
+export const createSet = <T = string>() => {
+  return new Set<T>()
+}
+
+const numberSet = createSet<number>()
+const otherStringSet = createSet() // if i do not pass anything it is string
+
+// 10 use generics to link from external libraries
+import { z } from 'zod'
+
+const makeZodSafeFetch = <TData>(
+  url: string,
+  schema: z.Schema<TData>,
+): Promise<TData> => {
+  return fetch(url).then(res => res.json()).then((res) => {
+    return schema.parse(res)
+  })
+}
+
+// i am repeating, how to improve?
+const resultZod = makeZodSafeFetch('/api/endpoint', z.object(
+  {
+    firstName: z.string(),
+    lastName: z.string(),
+  },
+)).then((res) => {
+  console.log(res)
+})
+
+export { }
